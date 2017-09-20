2c9378ffdba27ccf775475b8c7c03f5af01e9c4b
Fernando Olguin <f.olguin.ch@gmail.com>
2017-09-20 16:49:12 +0800

    Create a logging manager

---
+++ logger.py
@@ -0,0 +1,42 @@
+import logging
+
+def get_logger(name, file_name='debug.log', **kwargs):
+    """Creates a new logger.
+
+    Parameters:
+        name (str): name of the logger.
+        file_name (str): file name of the log.
+    Keywords:
+        filelevel (default=logging.DEBUG): logging to file level.
+        stdoutlevel (default=logging.INFO): logging to std output level.
+        filefmt (str, default=%(asctime)s - %(name)s - %(levelname)s: %(message)s): 
+            logging to file message format.
+        stdoutfmt (str, default=%(levelname)s: %(message)s): 
+            logging to std output message format.
+        maxBytes (int, default=5MB): maximum size of logging file in bytes.
+        backupCount (int, default=5): maximum number of log files to rotate.
+    """
+    # Create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(kwargs.get('filelevel', logging.DEBUG))
+
+    # File handler
+    fh = logging.handlers.RotatingFileHandler(file_name,
+            maxBytes=kwargs.get('maxBytes',5242880),
+            backupCount=kwargs.get('backupCount',5))
+    fh.setLevel(kwargs.get('filelevel', logging.DEBUG))
+    fh.setFormatter(logging.Formatter(kwargs.get('filefmt', 
+        '%(asctime)s - %(name)s - %(levelname)s: %(message)s')))
+
+    # Stream handler
+    sh = logging.StreamHandler()
+    sh.setLevel(kwargs.get('stdoutlevel', logging.INFO))
+    sh.setFormatter(logging.Formatter(kwargs.get('stdoutfmt', 
+        '%(levelname)s: %(message)s')))
+
+    # Register handlers
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+
+    return logger
+
