200714b1115a037a13b430dadeb0e79b027300a4
Fernando Olguin <f.olguin.ch@gmail.com>
2017-11-13 18:12:11 +0800

    Change ConfigParser to myConfigParser

--- argparse_actions.py
+++ argparse_actions.py
@@ -1,12 +1,13 @@
-import argparse
-from configparser import ConfigParser, ExtendedInterpolation
+import os, argparse
+from configparser import ExtendedInterpolation
+from myconfigparser import myConfigParser
 
 class LoadConfig(argparse.Action):
     """Action class for loading a configuration file in argparse"""
 
     def __call__(self, parser, namespace, values, option_string=None):
-        config = ConfigParser(interpolation=ExtendedInterpolation())
+        assert os.path.isfile(values)
+        config = myConfigParser(interpolation=ExtendedInterpolation())
         config.read(values)
-        print 'Read config %s' % values
         setattr(namespace, self.dest, config)
 
