b33e4eb18cda8c6d9a42850d1c08df834d01cb93
Fernando Olguin <f.olguin.ch@gmail.com>
2017-11-13 18:08:56 +0800

    Extend ConfigParser to get astropy quantities

---
+++ myconfigparser.py
@@ -0,0 +1,13 @@
+from configparser import ConfigParser
+
+import numpy as np
+import astropy.units as u
+
+class myconfigparser(ConfigParser):
+
+    def getquantity(self, *args, **kwargs):
+        val = self.get(*args, **kwargs).split()
+        if len(val)==2:
+            return float(val[0]) * u.Unit(val[1])
+        else:
+            return np.array(val[:-1], dtype=float) * u.Unit(val[-1])
