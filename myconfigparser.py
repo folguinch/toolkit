dcad3d5099bf8200b0b80a5695e7fd7bf10f2952
Fernando Olguin <f.olguin.ch@gmail.com>
2017-11-14 11:51:00 +0800

    Update documentation

--- myconfigparser.py
+++ myconfigparser.py
@@ -4,8 +4,14 @@ import numpy as np
 import astropy.units as u
 
 class myConfigParser(ConfigParser):
+    """Extend the configparser.ConfigParser behaviour.
+    """
 
     def getquantity(self, *args, **kwargs):
+        """Return an ``astropy.Quantity`` from the parser data.
+
+        Input parameters are the same as for ``parser.get(*args,**kwargs)``
+        """
         val = self.get(*args, **kwargs).split()
         if len(val)==2:
             return float(val[0]) * u.Unit(val[1])
