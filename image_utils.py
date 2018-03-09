6fd019d3fe383db827fc3f1c848272c1767c66fe
Fernando Olguin <f.olguin.ch@gmail.com>
2018-03-09 12:48:18 +0800

    Add rotation function

--- image_utils.py
+++ image_utils.py
@@ -1,9 +1,11 @@
 import numpy as np
+from astropy.io import fits
 import astropy.units as u
 import scipy.ndimage as scimage
 from scipy.stats import linregress
 
 from .logger import get_logger
+from .rotate import rotate, rotate_coord_clockwise
 
 """Functions for working with fits files.
 """
@@ -37,7 +39,6 @@ def get_coord_axes(img, pixsizes=(None,None)):
         img (astropy.hdu): input hdu.
     """
     return [get_coord_axis(img, i, pixsize=pixsizes[i-1]) for i in [1,2]]
-
 
 def center_of_mass(img, axis=None, mask=None, x=None, y=None):
     """Calculates the center of mass of img.
@@ -153,3 +154,118 @@ def lin_vel_gradient(img, sigma=0., nsigma=5, pixsizes=(None,None),
     logger.info('\tError = %s', stderr.to(u.km/u.s/u.arcsec))
 
     return slope, stderr, intercept, newx*newx_units, center*newy_units
+
+def add_stokes(img):
+    """Add the Stokes axis.
+
+    Parameters:
+        fits (astropy.io.fits): fits file.
+    """
+    if img.data.ndim==4:
+        return
+    elif img.data.ndim==3:
+        newfits = np.array([img.data])
+        assert newfits.ndim==4
+        newheader = img.header
+        newheader['NAXIS'] = 4
+        newheader['NAXIS4'] = 1
+        newheader['CTYPE4'] = 'STOKES'
+        newheader['CUNIT4'] = ''
+        newheader['CRPIX4'] = 1
+        newheader['CDELT4'] = 1
+        newheader['CRVAL4'] = 1
+        hdu = fits.PrimaryHDU(newfits)
+        hdu.header = newheader
+        return hdu
+    else:
+        newfits = np.array([img.data])
+        assert newfits.ndim==3
+        newheader = img.header
+        newheader['NAXIS'] = 3
+        newheader['NAXIS4'] = 1
+        newheader['CTYPE3'] = 'STOKES'
+        newheader['CUNIT3'] = ''
+        newheader['CRPIX3'] = 1
+        newheader['CDELT3'] = 1
+        newheader['CRVAL3'] = 1
+        hdu = fits.PrimaryHDU(newfits)
+        hdu.header = newheader
+        return hdu
+
+def rotate_cube(img, angle, source, centre=(0,0), mode='bilinear', **kwargs):
+    """Rotate image cube.
+
+    Parameters
+        angle: angle of rotation in degrees.
+        centre: centre of rotation.
+        mode: interpolation mode.
+    """
+    data = fits.open(img)[0]
+    data = add_stokes(data)
+    old_centre = data.data.shape[2]/2.-.5, data.data.shape[3]/2.-.5
+
+    # Rotate the cube
+    new_img = None
+    naxis4 = int(data.header['NAXIS4'])
+    for i in range(naxis4): 
+        aux = None
+        for j, slc in enumerate(data.data[i]):
+            rotated = rotate(slc, angle, centre=centre, mode=mode)
+            if aux is None:
+                aux = np.array([rotated])
+            else:
+                aux = np.append(aux, [rotated], axis=0)
+        if new_img is None:
+            new_img = np.array([aux])
+        else:
+            new_img = np.append(new_img, [aux], axis=0)
+
+    # Create a new fits file
+    hdu = fits.PrimaryHDU(new_img)
+    hdu.header = data.header
+    hdu.header['COMMENT'] = 'Rotated %.3f deg, center %i, %i' % \
+            ((angle,)+centre)
+    
+    # Redefine the reference pixel
+    #if 'CRPIX1' in self.header:
+    #    xref, yref = rotate_coord_clockwise(self['CRPIX1']-1,
+    #                                        self['CRPIX2']-1, angle, 
+    #                                        centre=old_centre[::-1])
+    #    xref += new_img.shape[3]/2. - .5
+    #    yref += new_img.shape[2]/2. - .5
+    #    if kwargs.get('ref_pos'):
+    #        ref_pos = kwargs['ref_pos']
+    #    elif self.header.get('CRVAL1') and self.header.get('CRVAL2'):
+    #        ref_pos = (self['CRVAL1'], self['CRVAL2'])
+    #    else:
+    #        ref_pos = None
+    #    self.set_refpix(xref+1, yref+1, ref_pos)
+    #if kwargs.get('centre_pos') is not None:
+    #    self.set_centrepos(kwargs['centre_pos'])
+    #elif kwargs.get('max_pos') is not None:
+    #    self.set_maxpos(kwargs['max_pos'])
+    #elif kwargs.get('set_pos') is not None:
+    #    self.set_refpix(*kwargs['set_pos'])
+    hdu.header['CRPIX1'] = aux.shape[2]/2. + .5
+    hdu.header['CRPIX2'] = aux.shape[1]/2. + .5
+    hdu.header['CRVAL1'] = source.position.ra.to(u.deg).value
+    hdu.header['CRVAL2'] = source.position.dec.to(u.deg).value
+
+    # Redifine header rotation
+    #if 'CROTA2' in hdu.header:
+    #    #if hdu.header['CROTA2']==angle:
+    #    self.logger.info('Deleting rotation keywords from header')
+    try:
+        del hdu.header['CROTA2']
+        del hdu.header['CROTA1']
+    except KeyError:
+        pass
+        #else:
+        #    self.logger.info('Changing rotation keywords from header')
+        #    hdu.header['CROTA2'] = hdu.header['CROTA2']-angle
+    #else:
+    #    self.logger.info('Setting rotation header keywords')
+    #    hdu.header['CROTA1'] = 0
+    #    hdu.header['CROTA2'] = angle
+
+    return hdu
