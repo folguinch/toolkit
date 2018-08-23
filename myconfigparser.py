4a00921e3face24bf207a9a7ae2db52f7c9c595c
Fernando Olguin <f.olguin.ch@gmail.com>
2018-08-23 10:56:41 +0800

    Change param getter
    Now it accounts for changes of the parameter values into the proper type.

--- myconfigparser.py
+++ myconfigparser.py
@@ -14,13 +14,26 @@ class myConfigParser(ConfigParser):
         Input parameters are the same as for ``parser.get(*args,**kwargs)``
         """
         val = self.get(*args, **kwargs)
+
+        # If the value was changed then it may be already a quantity
+        if hasattr(val, 'unit'):
+            return val
+        
+        # Fallback values
         if val is None or len(val)==0:
             return val
         else:
-            val= val.split()
-        if len(val)==2:
+            val = val.split()
+
+        # Convert string to quantity
+        if len(val)==1:
+            # Dimesionless
+            return float(val[0])
+        elif len(val)==2:
+            # Single quantity
             return float(val[0]) * u.Unit(val[1])
         else:
+            # Array of values
             return np.array(val[:-1], dtype=float) * u.Unit(val[-1])
 
     def getlist(self, *args, **kwargs):
@@ -29,34 +42,46 @@ class myConfigParser(ConfigParser):
         if val is None:
             return val
         else:
-            if ' ' not in val:
-                return [val]
-            else:
+            try:
                 return val.split()
+            except AttributeError:
+                return list(val)
+            #if ' ' not in val:
+            #    return [val]
+            #else:
+            #    return val.split()
 
     def getfloatlist(self, *args, **kwargs):
         """Return a list of float values."""
-        val = self.get(*args, **kwargs)
+        #val = self.get(*args, **kwargs)
+        #if val is None:
+        #    return val
+        #else:
+        #    if ' ' not in val:
+        #        val = [val]
+        #    else:
+        #        val= val.split()
+        val = self.getlist(*args, **kwargs)
         if val is None:
             return val
         else:
-            if ' ' not in val:
-                val = [val]
-            else:
-                val= val.split()
-        return map(float, val)
+            return map(float, val)
 
     def getintlist(self, *args, **kwargs):
         """Return a list of int values."""
-        val = self.get(*args, **kwargs)
+        #val = self.get(*args, **kwargs)
+        #if val is None:
+        #    return val
+        #else:
+        #    if ' ' not in val:
+        #        val = [val]
+        #    else:
+        #        val= val.split()
+        val = self.getlist(*args, **kwargs)
         if val is None:
             return val
         else:
-            if ' ' not in val:
-                val = [val]
-            else:
-                val= val.split()
-        return map(int, val)
+            return map(int, val)
 
     def getpath(self, *args, **kwargs):
         """Return a real path"""
