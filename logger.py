4d40964ff9d6a540e05897926b17d51a5c8a2d83
Fernando Olguin <f.olguin.ch@gmail.com>
2020-01-02 13:26:03 +0800

    Improve compatibility with argparse_tools

--- logger.py
+++ logger.py
@@ -1,7 +1,16 @@
-import os, logging
+import logging
 import logging.handlers
+import os
 
-def get_logger(name, file_name='debug.log', **kwargs):
+def get_level(args):
+    numeric_level = getattr(logging, args.loglevel[0].upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.loglevel[0])
+    return numeric_level
+
+def get_logger(name, file_name='debug.log', args=None, 
+        stdoutlevel=logging.INFO, filelevel=logging.DEBUG,
+        maxBytes=5242880, backupCount=5):
     """Creates a new logger.
 
     Parameters:
@@ -20,31 +29,38 @@ def get_logger(name, file_name='debug.log', **kwargs):
     # Create logger
     logger = logging.getLogger(name)
     if not len(logger.handlers):
-        logger.setLevel(kwargs.get('filelevel', logging.DEBUG))
+        logger.setLevel(filelevel)
 
         # File handler
         file_name = os.path.expanduser(file_name)
         filefmt = '%(asctime)s [%(levelname)s] - %(filename)s '+\
                 '(%(funcName)s:%(lineno)s): %(message)s'
         fh = logging.handlers.RotatingFileHandler(file_name,
-                maxBytes=kwargs.get('maxBytes',5242880),
-                backupCount=kwargs.get('backupCount',5))
-        fh.setLevel(kwargs.get('filelevel', logging.DEBUG))
-        fh.setFormatter(logging.Formatter(kwargs.get('filefmt', filefmt)))
-        #fh.setFormatter(logging.Formatter(kwargs.get('filefmt', 
-        #    '%(asctime)s - %(name)s - %(levelname)s: %(message)s')))
+                maxBytes=maxBytes, backupCount=backupCount)
+        fh.setLevel(filelevel)
+        fh.setFormatter(logging.Formatter(filefmt))
 
         # Stream handler
         sh = logging.StreamHandler()
-        sh.setLevel(kwargs.get('stdoutlevel', logging.INFO))
-        sh.setFormatter(logging.Formatter(kwargs.get('stdoutfmt', 
-            '%(levelname)s: %(message)s')))
+        if args is not None:
+            sh.setLevel(get_level(args))
+        else:
+            sh.setLevel(stdoutlevel)
+        if args is not None and args.loglevel[0].lower()=='debug':
+            streamfmt = '%(levelname)s - %(filename)s (%(funcName)s): %(message)s'
+        else:
+            streamfmt = '%(levelname)s: %(message)s'
+        sh.setFormatter(logging.Formatter(streamfmt))
 
         # Register handlers
         logger.addHandler(fh)
         logger.addHandler(sh)
 
-    return logger
+    if args is not None:
+        args.log = logger
+        return args
+    else:
+        return logger
 
 def logger_from_config(file_name):
    pass 
