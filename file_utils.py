235f4eef11559c418ef8030a74fb745773c36430
Fernando Olguin <f.olguin.ch@gmail.com>
2020-05-08 15:05:20 +0800

    Create rsync utility

--- file_utils.py
+++ file_utils.py
@@ -1,3 +1,15 @@
+import os
+import subprocess
+
 def write_txt(text, filename, mode='w'):
     with open(filename, mode) as out:
         out.write(text)
+
+def rsync(origin, destination, *args):
+    rsynccmd = 'rsync'
+    flags = '-auvr'
+    rsyncargs = [rsynccmd, flags] + list(args) + [origin, destination]
+    subprocess.call(rsyncargs)
+    
+    if not os.path.exists(destination) and not os.path.isfile(destination):
+        raise OSError('rsync failed')
