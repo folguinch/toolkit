edc4584536f1d29782cd68665f2a3c221d5ecfb8
Fernando Olguin <f.olguin.ch@gmail.com>
2024-11-19 10:56:20 +0900

    Identify submasks of connected elements

--- toolkit/astro_tools/masking.py
+++ toolkit/astro_tools/masking.py
@@ -6,16 +6,17 @@ from astropy.io import fits
 from astropy.wcs.utils import skycoord_to_pixel
 import astropy.units as u
 import numpy as np
+import numpy.typing as npt
 import matplotlib.pyplot as plt
 import scipy.ndimage as ndimg
 
 from ..maths import quick_rms
 
 def emission_mask(*images: fits.PrimaryHDU,
-                  initial_mask: Optional[np.array] = None,
+                  initial_mask: Optional[npt.ArrayLike] = None,
                   threshold: Optional[Sequence[u.Quantity]] = None,
                   nsigma: float = 5,
-                  log: Callable = print) -> np.array:
+                  log: Callable = print) -> npt.ArrayLike:
     """Generate a mask from images with emission over a threshold.
 
     An inital mask with the same dimessions as the data in args can be given.
@@ -25,11 +26,11 @@ def emission_mask(*images: fits.PrimaryHDU,
     `args` and the threshold will be `nsigma*rms`.
 
     Args:
-      images: input images.
-      initial_mask: optional; initial mask.
-      threshold: optional; emission threshold per image.
-      nsigma: optional; number of rms levels for threshold.
-      log: optional; logging function.
+      images: Input images.
+      initial_mask: Optional. Initial mask.
+      threshold: Optional. Emission threshold per image.
+      nsigma: Optional. Number of rms levels for threshold.
+      log: Optional. Logging function.
 
     Returns:
       A mask array.
@@ -69,12 +70,12 @@ def emission_mask(*images: fits.PrimaryHDU,
 
     return mask
 
-def mask_structures(mask: np.array, min_area: Optional = None) -> Tuple:
+def mask_structures(mask: npt.ArrayLike, min_area: Optional = None) -> Tuple:
     """Identify mask structures and index them.
 
     Args:
-      maks: input mask.
-      min_area: optional; minimum area in pixels of mask structures.
+      maks: Input mask.
+      min_area: Optional. Minimum area in pixels of mask structures.
 
     Returns:
       A mask array with small structures filtered out.
@@ -97,6 +98,50 @@ def mask_structures(mask: np.array, min_area: Optional = None) -> Tuple:
 
     return mask, labels, nlabels
 
+def split_mask_structures(mask: npt.ArrayLike,
+                          min_area: Optional = None,
+                          padding: float = 0.,
+                          ) -> Tuple[npt.ArrayLike, List[npt.ArrayLike]]:
+    """Identify mask structures and generate a mask for each.
+
+    The generated masks have the same dimensions as the input mask.
+
+    Args:
+      maks: Input mask.
+      min_area: Optional. Minimum area in pixels of mask structures.
+      padding: Optional. Border as a fraction of length of each axis range.
+
+    Returns:
+      A mask array with small structures filtered out.
+      A list with all the sub-masks.
+    """
+    # Get final mask and structures
+    mask, labels, _ = mask_structures(mask, min_area=min_area)
+
+    # Identify mask structures
+    objects = ndimage.find_objects(labels)
+
+    # Iterate over objects and generate individual masks
+    shape = mask.shape
+    submasks = []
+    for slcy, slcx in objects:
+        # Padding
+        padx = abs(slcx.start - slcx.stop) * padding
+        pady = abs(slcy.start - slcy.stop) * padding
+
+        # Valid indices
+        slc = (slice(max(0, slcy.start - pady),
+                     min(shape[0], slcy.stop + pady)),
+               slice(max(0, slcx.start - padx),
+                     min(shape[1], slcx.stop + padx)))
+
+        # New mask
+        newmask = np.zeros(shape, dtype=bool)
+        newmask[slc] = True
+        submasks.append(newmask)
+
+    return mask, submasks
+
 def position_in_mask(position: 'astropy.coordinates.SkyCoord',
                      mask: np.array,
                      wcs: WCS) -> bool:
@@ -106,7 +151,7 @@ def position_in_mask(position: 'astropy.coordinates.SkyCoord',
     return mask[int(y), int(x)]
 
 def plot_mask(
-    mask: np.array,
+    mask: npt.ArrayLike,
     scatter: Optional[Sequence['astropy.coordinates.SkyCoord']] = None,
     wcs: Optional[WCS] = None,
     scatter_kwds: Optional[Dict] = None,
@@ -115,11 +160,11 @@ def plot_mask(
     """Create a plot of `mask`.
 
     Args:
-      mask: mask array.
-      scatter: optional; sky coordinates of scatter points.
-      wcs: optional; coordiante system to interpret the scatter points.
-      scatter_kwds: optional; keywords for `plt.scatter`.
-      kwargs: keywords for `plt.subplots`.
+      mask: Mask array.
+      scatter: Optional. Sky coordinates of scatter points.
+      wcs: Optional. Coordiante system to interpret the scatter points.
+      scatter_kwds: Optional. Keywords for `plt.scatter`.
+      kwargs: Otional. Keywords for `plt.subplots`.
 
     Returns:
       A tuple with the figure and axis.
